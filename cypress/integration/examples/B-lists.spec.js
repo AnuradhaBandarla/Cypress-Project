387762a1f01432ce66be23444720ca654a84a192
AnuradhaBandarla <52680492+AnuradhaBandarla@users.noreply.github.com>
2020-02-26 16:34:00 +0000

    Update B-lists.spec.js

--- cypress/integration/examples/B-lists.spec.js
+++ cypress/integration/examples/B-lists.spec.js
@@ -1,8 +1,9 @@
+import 'cypress-file-upload';
 ///<reference types = 'cypress'/>
 describe('lists', function()
 {it('upload lists in csv format', function()
 {
-    cy.visit("https://app.release.qa.pur3.net/");
+        cy.visit("https://app.release.qa.pur3.net/");
         cy.get('[p3-validator-bootstrap="username"] > .form-control').type('anuradha');
         cy.get('[p3-validator-bootstrap="password"] > .form-control').type('puretest123');
         cy.get('.btn.btn-success.btn-block.ng-binding').click();
@@ -14,10 +15,16 @@ describe('lists', function()
         //cy.get('[class="btn.btn-default.ng-binding.ng-scope":visible]').click();
         //cy.get('.uploader.text-center.default.ng-isolate-scope.not-uploading').click();
         //cy.get('[p3-select-allow-single="true"]').click();
+        const fileName = 'profile.json';
+        cy.fixture(fileName).then(fileContent => {
+          cy.get('.btn.btn-default.ng-binding.ng-scope').last().invoke('show')
+          .upload({ fileContent, fileName, mimeType: 'application/json'},{ subjectType: 'input', force: true },);
+        });
         cy.get('.file-select').click({force: true});
         //cy.get('[type="radio"]').first().check();
         cy.get('[type="checkbox"]').check();
-        
+        cy.get('.btn.btn-success.flow-action-btn.flow-next').click({force:true})
+      
         //cy.get('[name="consentConfirmed"]').check()
         //cy.get('[ng-transclude ng-click="triggerFileSelect()"]').click({force: true});
         //cy.get('span > .btn').click();
