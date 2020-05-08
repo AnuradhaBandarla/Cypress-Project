e854fb1e472c238030b5ebb354a8b16a4452bdf5
Anuradha Bandarla <anuradha.bandarla@office.pur3.net>
2020-05-08 21:34:06 +0100

    created smoke test suite 1

--- cypress/support/commands.js
+++ cypress/support/commands.js
@@ -7,12 +7,38 @@
 // commands please read more here:
 // https://on.cypress.io/custom-commands
 // ***********************************************
+<<<<<<< HEAD
 //
 //
 // -- This is a parent command --
 // Cypress.Commands.add("login", (email, password) => { ... })
 //
 //
+=======
+// -- This is a parent command --
+// Cypress.Commands.add("login", (email, password) => { ... })
+
+
+// ---below is a command used to login to V8---
+Cypress.Commands.add('login', (username, password) => 
+{
+  cy.get('input[name="username"]').type('anu');
+  cy.get('input[name="password"]').type('Puretest123');
+  cy.get('.btn.btn-success.btn-block.ng-binding').click();
+
+})
+
+//---below is a command  to finish the list upload process ---
+Cypress.Commands.add("finish_list_upload", () => { 
+  cy.get('[type="checkbox"]').click({ force: true });
+  cy.wait(2000);
+  cy.contains('Next').click({force:true});
+  cy.get('[id="select-email"]').select('Column A - anuradha.bandarla@pure360.com')
+  cy.wait(1000);
+  cy.contains('Next').click({force:true})
+  cy.wait(1000);
+  cy.contains('button', 'Finish').click({force:true}) })
+>>>>>>> created smoke test suite 1
 // -- This is a child command --
 // Cypress.Commands.add("drag", { prevSubject: 'element'}, (subject, options) => { ... })
 //
@@ -23,3 +49,9 @@
 //
 // -- This will overwrite an existing command --
 // Cypress.Commands.overwrite("visit", (originalFn, url, options) => { ... })
+
+import 'cypress-file-upload';
+
+
+  // Thus, cy.login can be used in all other files
+  
